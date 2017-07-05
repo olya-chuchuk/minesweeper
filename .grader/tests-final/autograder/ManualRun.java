4ec75f9b6082511d80fc04fa9750d3414feb7fca
Olga Chuchuk <olya2103chuchuk@gmail.com>
2017-07-05 18:28:03 +0300

    Removed folder

--- .grader/tests-final/autograder/ManualRun.java
+++
@@ -1,9 +0,0 @@
-package autograder;
-
-import java.io.IOException;
-
-public class ManualRun {
-    public static void main(String args[]) throws IOException {
-        TestUtil.startServer(false, "board_file_3");
-    }
-}
